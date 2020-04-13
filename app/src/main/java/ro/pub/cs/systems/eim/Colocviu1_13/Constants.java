03e14d4b604993f7588360491ca36e3de1189072
Vlad Vitan <vlad.vitan@gmail.com>
2020-04-13 11:33:15 +0300

    RezolvareB

---
+++ app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_13/Constants.java
@@ -0,0 +1,9 @@
+package ro.pub.cs.systems.eim.Colocviu1_13;
+
+
+public class Constants {
+    public static final String NORTH = "North";
+    public static final String SOUTH = "South";
+    public static final String EAST  = "East";
+    public static final String WEST  = "West";
+}
--- app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_13/MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_13/MainActivity.java
@@ -1,14 +1,120 @@
 package ro.pub.cs.systems.eim.Colocviu1_13;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private ButtonClickListener buttonClickListener = new ButtonClickListener();
+    private Button northButton;
+    private Integer northButtonCount;
+    private Button southButton;
+    private Integer southButtonCount;
+    private Button eastButton;
+    private Integer eastButtonCount;
+    private Button westButton;
+    private Integer westButtonCount;
+
+    private class ButtonClickListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            if (v.getId() == R.id.northButtonId) {
+                textView.setText(textView.getText() + " " + Constants.NORTH);
+
+                northButtonCount++;
+            }
+            if (v.getId() == R.id.southButtonId) {
+                textView.setText(textView.getText() + " " + Constants.SOUTH);
+
+                southButtonCount++;
+            }
+            if (v.getId() == R.id.eastButtonId) {
+                textView.setText(textView.getText() + " " + Constants.EAST);
+
+                eastButtonCount++;
+            }
+            if (v.getId() == R.id.westButtonId) {
+                textView.setText(textView.getText() + " " + Constants.WEST);
+
+                westButtonCount++;
+            }
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+
+        savedInstanceState.putString("textView", textView.getText().toString());
+        savedInstanceState.putInt("northButtonCount", northButtonCount);
+        savedInstanceState.putInt("southButtonCount", southButtonCount);
+        savedInstanceState.putInt("eastButtonCount", eastButtonCount);
+        savedInstanceState.putInt("westButtonCount", westButtonCount);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textViewId);
+        northButton = findViewById(R.id.northButtonId);
+        southButton = findViewById(R.id.southButtonId);
+        eastButton = findViewById(R.id.eastButtonId);
+        westButton = findViewById(R.id.westButtonId);
+
+        northButton.setOnClickListener(buttonClickListener);
+        southButton.setOnClickListener(buttonClickListener);
+        eastButton.setOnClickListener(buttonClickListener);
+        westButton.setOnClickListener(buttonClickListener);
+
+        northButtonCount = 0;
+        southButtonCount = 0;
+        eastButtonCount = 0;
+        westButtonCount = 0;
+
+        /* restore old state */
+        if (savedInstanceState != null) {
+            String oldTextViewContent = savedInstanceState.getString("textView");
+
+            if (oldTextViewContent != null) {
+                textView.setText(oldTextViewContent);
+            }
+
+            Integer oldNorthButtonCount = savedInstanceState.getInt("northButtonCount");
+            if (oldNorthButtonCount != null) {
+                northButtonCount = oldNorthButtonCount;
+            }
+
+            Integer oldSouthButtonCount = savedInstanceState.getInt("southButtonCount");
+            if (oldSouthButtonCount != null) {
+                southButtonCount = oldSouthButtonCount;
+            }
+            Integer oldEastButtonCount = savedInstanceState.getInt("eastButtonCount");
+            if (oldEastButtonCount != null) {
+                eastButtonCount = oldEastButtonCount;
+            }
+            Integer oldWestButtonCount = savedInstanceState.getInt("westButtonCount");
+            if (oldWestButtonCount != null) {
+                westButtonCount = oldWestButtonCount;
+            }
+
+        }
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
 }
